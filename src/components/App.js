388d137fc056481b1ece09d9fd532dedd1fff393
Lemony <lemonystudio@gmail.com>
2018-10-30 14:38:48 +0000

    containers

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,16 @@
 import React from 'react';
 import VotingButtonsContainer from '../containers/VotingButtonsContainer';
-import CountrySelector from './CountrySelector';
+import CountrySelectorContainer from '../containers/CountrySelectorContainer';
 
-class App extends React.Component {
-  constructor(props){
-    super(props);
+function App ({store}) {
 
-    const reduxState = this.props.store.getState();
-
-    this.state = {
-      selectedCountry: reduxState.country.selectedCountry,
-      countryListOpen: reduxState.country.countryListOpen
-    }
-  }
-
-  componentDidMount(){
-    const { store } = this.props;
-    store.subscribe( () => {
-      this.updateFromStore();
-    });
-  }
-
-  updateFromStore(){
-    const { store } = this.props;
-
-    const reduxState = store.getState();
-
-    this.setState({
-      selectedCountry: reduxState.country.selectedCountry,
-      countryListOpen: reduxState.country.countryListOpen
-    });
-  }
-
-  render(){
     return (
       <div>
-        <VotingButtonsContainer store={this.props.store} />
-        <CountrySelector
-          selectedCountry={this.state.selectedCountry}
-          countryListOpen={this.state.countryListOpen}
-          dispatch={this.props.store.dispatch}
-        />
+        <VotingButtonsContainer store={store} />
+        <CountrySelectorContainer store={store} />
       </div>
     )
-  }
+  
 }
 
 export default App;
--- src/components/CountrySelector.js
+++ src/components/CountrySelector.js
@@ -3,33 +3,9 @@ import cx from 'classnames';
 
 const countries = ['Belgium', 'France', 'Germany', 'Holland', 'Ireland', 'Italy', 'Luxemburg', 'Portugal', 'Spain'];
 
-function CountrySelector({ countryListOpen, selectedCountry, dispatch}){
+function CountrySelector({ countryListOpen, selectedCountry, handleBlur, handleFocus, selectCountry}){
 
-  function selectCountry(country){
-    dispatch({
-      type: 'SET_SELECTED_COUNTRY',
-      selectedCountry: country
-    });
-
-    dispatch({
-      type: 'SET_COUNTRY_LIST_OPEN',
-      countryListOpen: false
-    });
-  }
-
-  function handleFocus(event){
-    dispatch({
-      type: 'SET_COUNTRY_LIST_OPEN',
-      countryListOpen: true
-    });
-  }
-
-  function handleBlur(event){
-    dispatch({
-      type: 'SET_COUNTRY_LIST_OPEN',
-      countryListOpen: false
-    });
-  }
+  
 
   const listClasses = cx('country-input__list',  {
     'country-input__list--visible': countryListOpen
